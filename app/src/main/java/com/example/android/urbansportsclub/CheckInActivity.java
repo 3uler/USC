8d4b454f1b2d8137fc1c3b6c259f17187cdf0560
3uler <3uler@users.noreply.github.com>
2018-12-25 12:44:43 +0100

    Upgrade Gradle to v3.2.1 and adjust dependencies.

--- app/src/main/java/com/example/android/urbansportsclub/CheckInActivity.java
+++ app/src/main/java/com/example/android/urbansportsclub/CheckInActivity.java
@@ -1,14 +1,16 @@
 package com.example.android.urbansportsclub;
 
+import android.annotation.SuppressLint;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.widget.TextView;
+
 import com.example.android.urbansportsclub.util.DateTimeStringProvider;
 
-import org.w3c.dom.Text;
+import java.util.Objects;
 
 public class CheckInActivity extends AppCompatActivity {
 
@@ -27,17 +29,19 @@ public class CheckInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_in);
-        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Objects.requireNonNull(this.getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         this.getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_action_close);
 
+        DateTimeStringProvider dateTimeStringProvider = new DateTimeStringProvider();
         mDateTime = (TextView) findViewById(R.id.datetime_tv);
-        mDateTime.setText(DateTimeStringProvider.getDateTimeString());
+        mDateTime.setText(dateTimeStringProvider.getDateTimeString());
         mTimer = (TextView) findViewById(R.id.timer_tv);
         mStartTime = System.currentTimeMillis();
         mHandler = new Handler();
 
         // Timer runnable
         mTimeRunnable = new Runnable() {
+            @SuppressLint("DefaultLocale")
             @Override
             public void run() {
                 final long start = mStartTime;
